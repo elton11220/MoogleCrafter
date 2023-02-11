ceb658ebf3dc25668ad75bd3fa23e1c729950629
elton11220 <jonesbak@outlook.com>
2023-02-11 12:01:21 +0800

    feat: add color extension - rippleBackgroundColor

--- src/config/themes/extension.ts
+++ src/config/themes/extension.ts
@@ -2,10 +2,12 @@ export const extendedLightColors = {
   primaryContentText: '#1a1a1a',
   secondaryContentText: '#767676',
   tertiaryContentText: '#9e9e9e',
+  rippleBackgroundColor: 'rgba(0, 0, 0, .3)',
 };
 
 export const extendedDarkColors = {
   primaryContentText: '#d8d8d8',
   secondaryContentText: '#aaaaaa',
   tertiaryContentText: '#9e9e9e',
+  rippleBackgroundColor: 'rgba(255, 255, 255, .3)',
 };
