7f7b5d2e2b813f610be7723758e4e451cc1bbbc9
elton11220 <jonesbak@outlook.com>
2023-03-03 09:26:10 +0800

    feat: scroll the list to top when tapping on the active tab

--- src/components/GatheringList/index.tsx
+++ src/components/GatheringList/index.tsx
@@ -3,12 +3,15 @@ import useEorzeaTimer from '../../hooks/useEorzeaTimer';
 import GatheringItem from '../../components/GatheringItem';
 import {getTimeTableFromGatheringPoints} from '../../utils/eorzeaTime';
 import {px2DpY} from '../../utils/dimensionConverter';
-import {memo, useCallback} from 'react';
+import {memo, useCallback, useRef} from 'react';
 import type {FC} from 'react';
+import {useScrollToTop} from '@react-navigation/native';
 
 const GatheringList: FC<GatheringList.Props> = props => {
   const {data: gatheringItems} = props;
   const eorzeaTime = useEorzeaTimer();
+  const flatListRef = useRef<FlatList<AppGlobal.GatheringItem> | null>(null);
+  useScrollToTop(flatListRef);
   const renderItem: (info: {item: AppGlobal.GatheringItem}) => JSX.Element = ({
     item,
   }) => (
@@ -32,6 +35,7 @@ const GatheringList: FC<GatheringList.Props> = props => {
   );
   return (
     <FlatList
+      ref={flatListRef}
       data={gatheringItems}
       renderItem={renderItem}
       getItemLayout={getItemLayout}
