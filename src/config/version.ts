e636de31349dd90e95a203292d7316951d45a827
elton11220 <jonesbak@outlook.com>
2023-05-01 21:46:35 +0800

    build: update version info - v0.2.0-stable-230501

--- src/config/version.ts
+++ src/config/version.ts
@@ -8,8 +8,8 @@ type VersionInfo = {
 
 const versionInfo: VersionInfo = {
   appName: '莫古锤子',
-  versionSymbol: 3,
-  compositedVersionSymbol: '0.1.2-stable-230427',
+  versionSymbol: 4,
+  compositedVersionSymbol: '0.2.0-stable-230501',
   versionType: 'stable',
   resVersion: '2023.02.16.0000.0000-6.25-chs',
 };
